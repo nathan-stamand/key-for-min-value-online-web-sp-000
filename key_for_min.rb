e903781a65a76895152c4c6995c00220d8423137
Nathan St.Amand <nathansaint810@gmail.com>
2020-02-20 17:49:16 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,11 +2,12 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
+  winner = 0
   minimum = 8000000000000000000000000000
   name_hash.each do |key, value|
     if value < minimum
-      minimum = key
+      winner = key
     end
   end
-  minimum
+  winner
 end
